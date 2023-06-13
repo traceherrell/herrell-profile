fcce2ca7941d58b2e918c960f41ff5e5146147c4
Trace Herrell <trace.herrell@gmail.com>
2023-06-13 08:13:12 -0700

    prefetch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,19 +5,28 @@
 <header class="fixed white">
   <nav>
     <div class="tabs max">
-      <a class:active={$page.url.pathname == "/"} href="/">
+      <a
+        data-sveltekit-preload-data
+        class:active={$page.url.pathname == "/"}
+        href="/"
+      >
         <i>home</i>
         <span>home</span>
       </a>
 
       <a
+        data-sveltekit-preload-data
         class:active={$page.url.pathname.includes("/portfolio")}
         href="/portfolio"
       >
         <i>work</i>
         <span>portfolio</span></a
       >
-      <a class:active={$page.url.pathname == "/history"} href="/history">
+      <a
+        data-sveltekit-preload-data
+        class:active={$page.url.pathname == "/history"}
+        href="/history"
+      >
         <i>history</i>
         <span>history</span>
       </a>
